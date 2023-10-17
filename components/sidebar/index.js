97dc937d508e80aa800462b1cee4ce4ba07f6a16
tanerengiiin <tanerengin4444@gmail.com>
2023-10-17 21:01:01 +0300

    update

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -10,8 +10,7 @@ const Sidebar = () => {
     const [activeBtn, setActiveBtn] = useState("Dashboard")
     const { sidebarOpen, setSidebarOpen } = useSidebarContext()
     return (
-        <div className={`full-height sidebar ${!!sidebarOpen && "sidebarOpen"}`}>
-            
+        <div className={`sidebar ${!!sidebarOpen && "sidebarOpen"}`}>
             <div id='logo'>
                 <svg width="107" height="32" viewBox="0 0 107 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M20.7207 6.18211L14.9944 3.11148L3.46855 9.28678L0.579749 7.73444L14.9944 0L23.6242 4.62977L20.7207 6.18211ZM14.9996 12.3574L26.5182 6.1821L29.4216 7.73443L14.9996 15.4621L6.37724 10.8391L9.27337 9.28677L14.9996 12.3574ZM2.89613 16.572L0 15.0196V24.2656L14.4147 32V28.8953L2.89613 22.7132V16.572ZM11.5185 18.09L0 11.9147V8.81001L14.4147 16.5376V25.7904L11.5185 24.2312V18.09ZM24.2086 15.0194V11.9147L15.5788 16.5377V31.9998L18.475 30.4474V18.09L24.2086 15.0194ZM27.0969 22.7129V10.3623L30.0004 8.81V24.2653L21.3706 28.895V25.7904L27.0969 22.7129Z" fill="#212121" />
